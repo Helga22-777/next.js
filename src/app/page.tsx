79be1b1a02cc77f4d5e321557e93d06c8c3fd4ef
Helga22-777 <kisa.manenka@gmail.com>
2025-01-26 21:02:13 +0200

    css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './styles/globals.css';
 
 export default function Home() {
   return (
-      <main> </main>
-       
+      <main></main>
   );
 }
