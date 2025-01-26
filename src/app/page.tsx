030765f3c5378cedf9510f9eea071117b2b2b7cb
Helga22-777 <kisa.manenka@gmail.com>
2025-01-26 20:47:18 +0200

    mega-blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,9 @@
+import Image from "next/image";
 import styles from "./page.module.css";
-import course from "../../course.json";
-import { Lesson } from "./lesson";
 
-async function getAllCourses(): Promise<typeof course> {
-  return new Promise((resolve) => {
-     setTimeout(() => resolve(course),1000)
-  })
-}
-
-export default async function Home() {
-  const courses = await getAllCourses()
+export default function Home() {
   return (
-      <main className={styles.main}>
-        <h2>test</h2>
-          <ul>
-          {courses.lessons.map((item)=> {
-            return  <li key={item.title}><Lesson title={item.title} shortSummery={item.shortSummery} /></li>
-          }   
-            )}
-          </ul>
-      </main>
+      <main> </main>
+       
   );
 }
